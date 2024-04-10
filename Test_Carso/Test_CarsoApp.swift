6200f4a4d9d436f52c17da19beb4a8ad3f3c08de
alejandro.guerra-at-848515362594 <alejandro.guerra-at-848515362594>
2024-04-10 12:58:14 -0600

    Initial Commit

---
+++ Test_Carso/Test_CarsoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CarsoApp.swift
+//  Test_Carso
+//
+//  Created by Alejandro Guerra on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_CarsoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
